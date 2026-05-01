bd65a41685c5298525a1102e0a2535297deb2700
Likhitha <likhithadommati@gmail.com>
2026-05-01 21:28:50 +0530

    Configured WhiteNoise for static files

--- ProjectPulse/settings.py
+++ ProjectPulse/settings.py
@@ -25,6 +25,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'ethara_project.urls'
@@ -88,3 +90,4 @@ LOGOUT_REDIRECT_URL = 'home'
 
 ALLOWED_HOSTS = ['*']
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
